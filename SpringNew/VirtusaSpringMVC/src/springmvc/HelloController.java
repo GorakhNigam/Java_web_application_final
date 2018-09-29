cc32d155b00e5acb4ce1f717c3395604cc8aafd3
Gorakhnath Nigam <jighrt16@gmail.com>
2018-09-29 12:16:25 +0530

    Add files via upload

---
+++ SpringNew/VirtusaSpringMVC/src/springmvc/HelloController.java
@@ -0,0 +1,12 @@
+package springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "Welcome";
+	}
+}
